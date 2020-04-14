97a18479e7bba1439b75d1a6add0478535602bc3
mgalus <mgalus@dreamlab.pl>
2020-04-14 21:39:29 +0200

    homepage

--- views/common/newsletterForm.php
+++ views/common/newsletterForm.php
@@ -8,46 +8,49 @@ use app\components\mgcms\MgHelpers;
 
 ?>
 
-
-<section class="Section Section--white text-center animatedParent">
+<section class="Section text-center animatedParent Section--fixed-bg" style="background-image: url(./images/newsletter_bg.jpg);">
     <div class="container fadeIn animated">
-        <h2><?= Yii::t('db', 'Newsletter'); ?></h2>
         <div class="row">
-            <div class="col-lg-6 offset-lg-3">
-                <?= MgHelpers::getSettingTranslated('newsletter text 1', 'newsletter text 1') ?>
-            </div>
-        </div>
-        <div class="Newsletter animatedParent">
-            <?php $form = ActiveForm::begin(['id' => 'newsletter-form', 'class' => 'fadeIn animated']); ?>
-            <div class="Newsletter__inner">
-                <div class="Form__group form-group">
-                    <input
-                            class="Form__input form-control"
-                            placeholder="&nbsp;"
-                            id="phone"
-                            name="newsletterEmail"
-                            type="email"
-                            required
-                    />
-                    <label class="Form__label" for="phone"
-                    >Wpisz swój adres e-mail</label
-                    >
+            <div class="col-lg-10 offset-lg-1">
+                <h2><?= Yii::t('db', 'Newsletter'); ?></h2>
+                <p>
+                    <?= MgHelpers::getSettingTranslated('newsletter text 1', '<span class="Color--secondary">Bądz na bieżąco</span> Dopisz się do bazy inwestorów, aby otrzymywać informacjie o nowych projektach.<br>
+                    Gwarantujemy zero spamu i same konkrety!') ?>
+                </p>
+                <div class="Newsletter animatedParent">
+                    <?php $form = ActiveForm::begin(['id' => 'newsletter-form', 'class' => 'fadeIn animated']); ?>
+                    <div class="Newsletter__inner">
+                        <div class="Form__group form-group">
+                            <input
+                                    class="Form__input form-control"
+                                    placeholder="&nbsp;"
+                                    id="phone"
+                                    name="newsletterEmail"
+                                    type="email"
+                                    required
+                            />
+                            <label class="Form__label" for="phone"
+                            >Wpisz swój adres e-mail</label
+                            >
+                        </div>
+
+                    </div>
+                    <div class="Form__group form-group text-left">
+                        <input class="Form__checkbox" type="checkbox" id="agree-1"/>
+                        <label for="agree-1">
+                            <?= MgHelpers::getSettingTranslated('newsletter zgoda 1', 'newsletter zgoda 1') ?>
+                        </label>
+                    </div>
+                    <div class="Form__group form-group text-left">
+                        <input class="Form__checkbox" type="checkbox" id="agree-2"/>
+                        <label for="agree-2">
+                            <?= MgHelpers::getSettingTranslated('newsletter zgoda 2', 'newsletter zgoda 2') ?>
+                        </label>
+                    </div>
+                    <input class="btn btn-success lowercase" type="submit" value="<?= Yii::t('db', 'Sign in'); ?>"/>
+                    <?php ActiveForm::end(); ?>
                 </div>
-                <input class="btn btn-success" type="submit" value="ZAPISZ SIĘ"/>
             </div>
-            <div class="Form__group form-group text-left">
-                <input class="Form__checkbox" type="checkbox" id="agree-1"/>
-                <label for="agree-1">
-                    <?= MgHelpers::getSettingTranslated('newsletter zgoda 1', 'newsletter zgoda 1') ?>
-                </label>
-            </div>
-            <div class="Form__group form-group text-left">
-                <input class="Form__checkbox" type="checkbox" id="agree-2"/>
-                <label for="agree-2">
-                    <?= MgHelpers::getSettingTranslated('newsletter zgoda 2', 'newsletter zgoda 2') ?>
-                </label>
-            </div>
-            <?php ActiveForm::end(); ?>
         </div>
     </div>
 </section>
